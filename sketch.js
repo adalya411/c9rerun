7028f1e62b2fc11e238d09d00d62ded33122bcf6
adalya411 <88462159+adalya411@users.noreply.github.com>
2021-08-04 15:54:12 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box
+function setup() {
+  createCanvas(400,400);
+box = createSprite (200, 200, 50, 50)
+}
+
+function draw() 
+{
+
+background("yellow");
+if (keyDown("left")){
+box.x = box.x - 5
+}
+if (keyDown ("right")){
+  box.x = box.x + 5
+}
+if (keyDown ("down")){
+  box.y = box.y + 5
+}
+if (keyDown ("up")){
+  box.y = box.y - 5
+}
+drawSprites ()
+}
+
+
+
+
